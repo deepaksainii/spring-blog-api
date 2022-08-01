947b04f2444abe9a8c3c647a83aad4832249d6f1
Deepak Saini <deepak.saini@hcl.com>
2022-08-01 11:04:49 +0530

    jwt token

---
+++ src/main/java/com/springboot/blog/security/JwtTokenProvider.java
@@ -0,0 +1,57 @@
+package com.springboot.blog.security;
+
+import com.springboot.blog.exception.BlogAPIException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.swing.text.BadLocationException;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt-expiration-miliseconds}")
+    private int jwtExpirationInMs;
+
+    //generate token
+    public String generateToken(Authentication authentication){
+        String username     =  authentication.getName();
+        Date currentDate    = new Date();
+        Date expireDate     = new Date(currentDate.getTime() + jwtExpirationInMs);
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.HS256,jwtSecret)
+                .compact();
+        return token;
+    }
+
+    //get username from token
+    public String getUsernameFromJWT(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    // validate JWT token
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        }catch (SignatureException exception){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Invalid JWT Token");
+        }
+    }
+}
